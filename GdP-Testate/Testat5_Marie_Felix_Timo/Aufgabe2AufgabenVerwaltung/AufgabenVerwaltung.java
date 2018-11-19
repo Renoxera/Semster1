191ca2c9ff30cfbf7f43541bed1ca20d650c529e
Service <Service@Fiducia-GAD-Notebook.speedport.ip>
2018-11-19 22:44:04 +0100

    Fertigstellung, ggf. muss noch Bugfixing betrieben werden.

--- GdP-Testate/Testat5_Marie_Felix_Timo/Aufgabe2AufgabenVerwaltung/AufgabenVerwaltung.java
+++ GdP-Testate/Testat5_Marie_Felix_Timo/Aufgabe2AufgabenVerwaltung/AufgabenVerwaltung.java
@@ -2,10 +2,16 @@ package Aufgabe2AufgabenVerwaltung;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import de.hsw.gdp.helper.SimpleReader;
 
 public class AufgabenVerwaltung {
-	ArrayList<Aufgabe> alleAufgaben, erledigteAufgaben, unerledigteAufgaben;
+	protected ArrayList<Aufgabe>	alleAufgaben, erledigteAufgaben, unerledigteAufgaben;
 	
+	protected static int[]			spaltenbreite	= { 30, 8, 7 };
+	
+	/**
+	 * legt die in der AufgabenVerwaltung gespeicherten Listen an.
+	 */
 	public AufgabenVerwaltung() {
 		alleAufgaben = new ArrayList<Aufgabe>();
 		erledigteAufgaben = new ArrayList<Aufgabe>();
@@ -13,18 +19,232 @@ public class AufgabenVerwaltung {
 	}
 	
 	/**
-	 * @param name
-	 * @param erledigt
-	 * @param datum    Form: dd.mm.yyyy, hh:mm
-	 * @return
+	 * Methode, die dazu dient eine neue Aufgabe den Listen anzuhängen.
+	 * 
+	 * @param aufgabe  Name bzw. Beschreibung der Aufgabe
+	 * @param erledigt true|false|ja|nein|1|0. Andernfalls Fehler
+	 * @param datum    (tt.mm.jjjj,ss:mm) Andernfalls Fehler
+	 * @return true, wenn erfolgreich. false, wenn es zu einem Fehler gekommen ist.
 	 */
-	public Aufgabe erzeugeAufgabe(String name, boolean erledigt, String datum) {
-		
-		GregorianCalendar faelligkeitsdatum = stringToDatum(datum);
-		return new Aufgabe(name, erledigt, faelligkeitsdatum);
+	public boolean fuegeAufgabeHinzu(String aufgabe, String erledigt, String datum) {
+		return fuegeAufgabeDerListeHinzu(pruefeEingabeUndErzeugeAufgabe(aufgabe, erledigt, datum));
 	}
 	
-	public GregorianCalendar stringToDatum(String datum) {
+	/**
+	 * Interne Methode, die die eingegebenen Strings Überprüft und eine neue Aufgabe
+	 * mit den übergebenen Werten erzeugt.
+	 * 
+	 * @param aufgabe  Name bzw. Beschreibung der Aufgabe
+	 * @param erledigt Status der Aufgabe
+	 * @param datum    Fälligkeitsdatum in der Form tt.mm.jjjj,ss:mm
+	 * @return Aufgabe, wenn Eingaben zulässig. null, wenn Eingaben unzulässig.
+	 */
+	private Aufgabe pruefeEingabeUndErzeugeAufgabe(String aufgabe, String erledigt, String datum) {
+		GregorianCalendar faelligkeitsdatum = stringZuDatum(datum);
+		int pruefeEr = pruefeErledigt(erledigt);
+		boolean erle;
+		if (pruefeEr == 0) {
+			erle = true;
+		} else if (pruefeEr == 1) {
+			erle = false;
+		} else return null;
+		if (faelligkeitsdatum != null) return new Aufgabe(aufgabe, erle, faelligkeitsdatum);
+		else return null;
+	}
+	
+	/**
+	 * Interne Methode, die einen eingegebenen String überprüft, ob sie in ein
+	 * boolean umsetzbar ist.
+	 * 
+	 * @param eingabe String zur überprüfung
+	 * @return 0 - zulässige Eingabe von true. 1 - zulässige Eingabe von false. 2 -
+	 *         unzulässige Eingabe
+	 */
+	private int pruefeErledigt(String eingabe) {
+		if (eingabe.equals("true") || eingabe.equals("1") || eingabe.equals("ja")) {
+			return 0;
+		} else if (eingabe.equals("false") || eingabe.equals("0") || eingabe.equals("nein")) {
+			return 1;
+		} else {
+			System.err.println("Falsche Eingabe. Bitte [true|false|0|1|ja|nein] eingeben.");
+			return 2;
+		}
+	}
+	
+	/**
+	 * Interne Methode, die eine Aufgabe an die Liste aller Aufgaben und an die
+	 * passende Liste erleidgt oder unerledigt an.
+	 * 
+	 * @param aufgabe anzuhängende Aufgabe. Falls die Aufgabe = null ist, wird
+	 *                nichts angehängt.
+	 * @return true, wenn erfolgreich. false, falls nicht.
+	 */
+	private boolean fuegeAufgabeDerListeHinzu(Aufgabe aufgabe) {
+		if (aufgabe == null) return false;
+		anpassungLaengeTabelle(aufgabe);
+		alleAufgaben.add(aufgabe);
+		if (aufgabe.erledigt) {
+			erledigteAufgaben.add(aufgabe);
+		} else {
+			unerledigteAufgaben.add(aufgabe);
+		}
+		return true;
+	}
+	
+	/**
+	 * Ausgabe einer Liste nach angegeben Sortierkriterium.
+	 * 
+	 * @param wonach      1 - Fälligkeitsdatum, 2 - Nummer des Einfügen/Bearbeiten
+	 * @param welcheListe 1 - alle Aufgaben, 2 - erledigte Aufgaben, 3 - unerledigte
+	 *                    Aufgaben
+	 */
+	public void ausgabeNach(int wonach, int welcheListe) {
+		wonach--;
+		welcheListe--;
+		if (wonach == 0 || wonach == 1 && welcheListe >= 0 && welcheListe <= 2) {
+			welcheListe += wonach * 3;
+			switch (welcheListe) {
+				case 0:
+					ausgabeAufgabenNachId(this.alleAufgaben);
+					break;
+				case 1:
+					ausgabeAufgabenNachId(this.erledigteAufgaben);
+					break;
+				case 2:
+					ausgabeAufgabenNachId(this.unerledigteAufgaben);
+					break;
+				case 3:
+					ausgabeAufgabenNachDatum(this.alleAufgaben);
+					break;
+				case 4:
+					ausgabeAufgabenNachDatum(this.erledigteAufgaben);
+					break;
+				case 5:
+					ausgabeAufgabenNachDatum(this.unerledigteAufgaben);
+					break;
+			}
+		}
+	}
+	
+	/**
+	 * Löscht einen Eintrag anhand eines Index.
+	 * 
+	 * @param nummer Nummer anhand der Indizes in der Liste aller Aufgaben beginnend
+	 *               bei 0;
+	 * @return true, wenn ein Eintrag erfolgreich entfernt werden konnte, false,
+	 *         wenn es zu einem Fehler gekommen ist.
+	 */
+	public boolean loescheNachNummer(int nummer) {
+		if (nummer >= 0 && nummer < alleAufgaben.size()) {
+			if (alleAufgaben.get(nummer).erledigt) erledigteAufgaben.remove(alleAufgaben.get(nummer));
+			else unerledigteAufgaben.remove(alleAufgaben.get(nummer));
+			alleAufgaben.remove(nummer);
+			return true;
+		}
+		System.err.println("Eingabe außerhalb der Indizes. " + nummer);
+		return false;
+	}
+	
+	/**
+	 * @param aufgabe
+	 * @return
+	 */
+	public boolean loescheNachAufgabe(String aufgabe) {
+		ArrayList<Aufgabe> auswahl = new ArrayList<Aufgabe>();
+		alleAufgaben.stream().filter(a -> a.aufgabe.toLowerCase().contains(aufgabe.toLowerCase()))
+				.forEach(k -> auswahl.add(k));
+		if (auswahl.size() > 1) {
+			ausgabeDerListeMitAngepasstenIndizes(auswahl);
+			System.out.print("Welche Aufgabe genau (Nr): ");
+			int nr = new SimpleReader().liesZahl(0, auswahl.size() - 1);
+			loescheNachNummer(alleAufgaben.indexOf(auswahl.get(nr)));
+		} else if (auswahl.size() == 1) {
+			loescheNachNummer(alleAufgaben.indexOf(auswahl.get(0)));
+		} else System.out.println("Keine Aufgabe mit dem Teilstring.");
+		return false;
+	}
+	
+	/**
+	 * Methode zur Ändern eines Status einer Aufgabe.
+	 * 
+	 * @param eingabe 1 um einen unerledigten Eintrag zu ändern, 2 um einen
+	 *                erledigte Eintrag zu ändern.
+	 */
+	public void aenderStatus(int eingabe) {
+		if (eingabe == 1) {
+			aendereStatusIntern(unerledigteAufgaben);
+		} else if (eingabe == 2) {
+			aendereStatusIntern(erledigteAufgaben);
+		} else System.err.println("Falsche Eingabe. " + eingabe);
+	}
+	
+	private void aendereStatusIntern(ArrayList<Aufgabe> liste) {
+		int nr = 0;
+		if (liste.size() == 0) {
+			System.err.println("Kein Eintrag zum ändern vorhanden.");
+			return;
+		} else if (liste.size() > 1) {
+			ausgabeDerListeMitAngepasstenIndizes(liste);
+			System.out.print("Welche Aufgabe genau (Nr): ");
+			nr = new SimpleReader().liesZahl(0, liste.size() - 1);
+			
+		} else nr = 0;
+		liste.get(nr).erledigt = !liste.get(nr).erledigt;
+		if (liste == unerledigteAufgaben) {
+			erledigteAufgaben.add(liste.get(nr));
+			unerledigteAufgaben.remove(liste.get(nr));
+		} else {
+			unerledigteAufgaben.add(liste.get(nr));
+			erledigteAufgaben.remove(liste.get(nr));
+			
+		}
+	}
+	
+	/**
+	 * Druckt den Tabellenkopf in der Konsole aus.
+	 */
+	private void printHeader() {
+		int i;
+		for (i = 2; alleAufgaben.size() / Math.pow(10, i) > 1; i++);
+		System.out.printf("| %" + i + "s | %-" + spaltenbreite[0] + "s | %-" + spaltenbreite[1] + "s | %-"
+				+ spaltenbreite[2] + "s |%n", "Nr", "Datum", "Erledigt", "Aufgabe");
+	}
+	
+	/**
+	 * gibt die übergebene Liste mit den Indizes dieser Liste an
+	 * @param liste Liste, die ausgegeben werden soll.
+	 */
+	private void ausgabeDerListeMitAngepasstenIndizes(ArrayList<Aufgabe> liste) {
+		printHeader();
+		liste.stream().forEach(a -> a.printLine(liste));
+	}
+	
+	/**
+	 * gibt die übergene Liste mit den Indizes aller Aufgaben aus. Die Ausgabe ist nach der Reihnfolge aller Aufgaben sortiert.
+	 * @param liste Liste, die ausgegeben werden soll.
+	 */
+	private void ausgabeAufgabenNachId(ArrayList<Aufgabe> liste) {
+		printHeader();
+		liste.stream().forEach(a -> a.printLine());
+	}
+	
+	/**
+	 * gibt die übergene Liste mit den Indizes aller Aufgaben aus. Die Ausgabe ist nach der Reihnfolge ihres Fälligkeitsdatum sortiert.
+	 * @param liste Liste, die ausgegeben werden soll.
+	 */
+	private void ausgabeAufgabenNachDatum(ArrayList<Aufgabe> liste) {
+		printHeader();
+		liste.stream().sorted().forEach(a -> a.printLine());
+	}
+	
+	/**
+	 * Wandelt ein in der Form (tt.mm.jjjj,ss:mm) eingegebenes Datum in ein Datum
+	 * Objekt der Klasse GregorianCalendar aus.
+	 * 
+	 * @param datum Datum in der obengenannten Form
+	 * @return Objekt der Klasse Gregorian, sofern ein valieds
+	 */
+	private static GregorianCalendar stringZuDatum(String datum) {
 		datum = datum.replaceAll("[^0-9,.:]", "");
 		if (datum.matches("[0-9]{1,2}[.][0-9]{1,2}[.][0-9]{1,4}[,][0-9]{1,2}[:][0-9]{1,2}")) {
 			String date = datum.split(",")[0];
@@ -39,17 +259,23 @@ public class AufgabenVerwaltung {
 			int stunde = Integer.parseInt(timeA[0]);
 			int minute = Integer.parseInt(timeA[1]);
 			if (pruefeValiedenTag(tag, monat, jahr) && (stunde >= 0 && stunde < 24) && (minute >= 0 && minute < 60)) {
-				GregorianCalendar d = new GregorianCalendar(jahr, monat-1, tag, stunde, minute);
-				return d;
+				return new GregorianCalendar(jahr, monat - 1, tag, stunde, minute);
 			}
 		}
-		System.out.println("Fehler in " + datum);
-		
-		// TODO Auto-generated method stub
+		System.err.println("Fehler in der Eingabe des Datums " + datum);
 		return null;
 	}
 	
-	private boolean pruefeValiedenTag(int tag, int monat, int jahr) {
+	/**
+	 * Interne Methode, die Überprüft, ob die übergebenen parameter ein valider Tag
+	 * im Jahr sind.
+	 * 
+	 * @param tag   Tag im Monat beginnend bei 1
+	 * @param monat Monat im Jahr beginnend bei 1
+	 * @param jahr  Jahr beginnend bei 0 für 0 n. Chr.
+	 * @return true, wenn es ein valider Tag ist
+	 */
+	private static boolean pruefeValiedenTag(int tag, int monat, int jahr) {
 		return (jahr >= 0) && (tag > 0)
 				&& (((monat > 0 && monat <= 7) && (((monat == 2 && jahr % 4 == 0) && (tag <= 29))
 						|| ((monat == 2 && jahr % 4 != 0) && (tag <= 28)) || ((monat % 2 == 1) && (tag <= 31))
@@ -59,28 +285,64 @@ public class AufgabenVerwaltung {
 		
 	}
 	
-	public void fuegeAufgabeHinzu(Aufgabe aufgabe) {
-		alleAufgaben.add(aufgabe);
-		if (aufgabe.istErledigt()) {
-			erledigteAufgaben.add(aufgabe);
-		} else {
-			unerledigteAufgaben.add(aufgabe);
+	/**
+	 * Prüft ob die Spaltenbreite für die in <code>spaltenbreite</code>
+	 * gespeicherten Breiten erhöht werden müssen, da ein breiterer String in den
+	 * Kontakten aufgenommen wurde.
+	 * 
+	 * @param aufgabeA
+	 */
+	private static void anpassungLaengeTabelle(Aufgabe aufgabe) {
+		String[] aufgabeA = { "" + aufgabe.faelligkeitsdatum.getTime(), "" + aufgabe.erledigt, aufgabe.aufgabe };
+		for (int i = 0; i < aufgabeA.length && i < spaltenbreite.length; i++) {
+			if (aufgabeA[i].length() > spaltenbreite[i]) {
+				spaltenbreite[i] = aufgabeA[i].length();
+			}
 		}
 	}
 	
-	class Aufgabe {
+	/**
+	 * Interne Klasse, in der die benötigten Daten für eine Aufgabe gespeichert
+	 * werden. Integration des Interface Comparable, damit Aufgaben nach ihrem
+	 * Fälligkeitsdatum sortiert weden können.
+	 * 
+	 * @author Marie Salomon, Felix Franz, Timo Reitmann
+	 */
+	class Aufgabe implements Comparable<Aufgabe> {
 		private boolean				erledigt	= false;
 		private String				aufgabe;
 		private GregorianCalendar	faelligkeitsdatum;
+		private int					id;
 		
-		public Aufgabe(String name, boolean erledigt, GregorianCalendar datum) {
-			this.aufgabe = name;
+		public Aufgabe(String aufgabe, boolean erledigt, GregorianCalendar faelligkeitsdatum) {
+			this.aufgabe = aufgabe;
 			this.erledigt = erledigt;
-			this.faelligkeitsdatum = datum;
+			this.faelligkeitsdatum = faelligkeitsdatum;
 		}
 		
-		public boolean istErledigt() {
-			return erledigt;
+		/**
+		 * Druckt den Kontakt in einer Zeile aus. Ausgabe ist an Tabellenkopf angepasst
+		 * und ein vorheriges Aufrufen ist sinnvoll.
+		 */
+		public void printLine() {
+			printLine(alleAufgaben);
+		}
+		/**
+		 * Gibt diese Aufgabe mit den Indizes 
+		 * @param liste
+		 */
+		public void printLine(ArrayList<Aufgabe> liste) {
+			int i;
+			id = liste.indexOf(this);
+			for (i = 2; alleAufgaben.size() / Math.pow(10, i) > 1; i++);
+			System.out.printf("| %0" + i + "d | %-" + spaltenbreite[0] + "s | %-" + spaltenbreite[1] + "s | %-"
+					+ spaltenbreite[2] + "s |%n", id, faelligkeitsdatum.getTime(), erledigt, aufgabe);
+		}
+		
+		@Override
+		public int compareTo(Aufgabe o) {
+			return this.faelligkeitsdatum.compareTo(o.faelligkeitsdatum);
 		}
 	}
+	
 }
--- GdP-Testate/Testat5_Marie_Felix_Timo/Aufgabe2AufgabenVerwaltung/Main.java
+++ GdP-Testate/Testat5_Marie_Felix_Timo/Aufgabe2AufgabenVerwaltung/Main.java
@@ -1,8 +1,101 @@
 package Aufgabe2AufgabenVerwaltung;
 
+import de.hsw.gdp.helper.SimpleReader;
+
 public class Main {
 	public static void main(String[] args) {
-		AufgabenVerwaltung t = new AufgabenVerwaltung();
-		System.out.println(t.stringToDatum("1.12.50,20:20").MINUTE);
+		AufgabenVerwaltung a = new AufgabenVerwaltung();
+//		beispielBelegung(a);
+		boolean schleife = true;
+		int eingabe;
+		do {
+			System.out.println(
+					"Was möchtest du machen? \n [1] - Aufgabe erfassen \n [2] - Ausgabe Aufgaben \n [3] - Aufgabenstatus ändern \n [4] - Aufgabe löschen \n [5] - Programm beenden");
+			SimpleReader reader = new SimpleReader();
+			switch (reader.liesZahl(1, 5)) {
+				case 1:
+					eingabeUndErzeugung(reader, a);			
+					break;
+				case 2:
+					System.out.print(" [0] - alle \n [1] - erledigte \n [2] - unerledigte\n Eingabe: ");
+					int auswahl1 = reader.liesZahl(0,2);
+					System.out.print(" Sortiert nach:\n [0] - Datum\n [1] - ID: ");
+					int auswahl2 = reader.liesZahl(0,1);
+					a.ausgabeNach(auswahl2, auswahl1);
+					break;
+				case 3:
+					System.out.println("Möchtest du den Status einer Aufgabe auf\n [0] - von unerledigt auf erledigt oder\n [1] - von erledigt auf unerledigt setzen? ");
+					eingabe = reader.liesZahl(0,1);
+  					a.aenderStatus(eingabe);
+					break;
+				case 4:
+					System.out.print(" [0] - Nach Name\n [1] - Nach ID\n");
+					String e = "";
+					switch (reader.liesZahl(0, 1)) {
+						case 0:
+							System.out.print("Namen oder Teilnamen eingeben: ");
+							e=reader.liesText();
+							a.loescheNachAufgabe(e);
+							break;
+						case 1:
+							System.out.print("ID der Aufgabe eingeben: ");
+							int i=reader.liesZahl(0);
+							a.loescheNachNummer(i);
+							
+							break;
+						default:
+							break;
+					}
+					break;
+				case 5:
+					schleife = false;
+					break;
+				default:
+					System.out.println("Eingabe konnte nicht verarbeitet werden. Bitte wiederholen. ");
+			}
+			System.out.println();
+		} while (schleife); 
+		
 	}
+
+	private static void eingabeUndErzeugung(SimpleReader reader, AufgabenVerwaltung t) {
+		String aufgabe, datum, erledigt;
+		System.out.print("Aufgabe: ");
+		aufgabe = reader.liesText();
+		System.out.print("Datum [tt.mm.jjjj,ss:mm]: ");
+		datum = reader.liesText();
+		System.out.print("Erledigt [true|false|0|1]: ");
+		erledigt = reader.liesText();
+		
+		
+		if (t.fuegeAufgabeHinzu(aufgabe, erledigt, datum))
+			System.out.println("\n Kontakt wurde erfolgreich angelegt.");
+		else System.out.println("\n Kontakt konnte nicht angelegt werden.");
+	}
+	
+	public static void beispielBelegung(AufgabenVerwaltung a) {
+
+		a.fuegeAufgabeHinzu("", "1", "");
+		a.fuegeAufgabeHinzu("Test", "1", "");
+		a.fuegeAufgabeHinzu("1223456789", "1", "1");
+		a.fuegeAufgabeHinzu("äasdfj", "1", "1");
+		a.fuegeAufgabeHinzu("Test1", "0", "0.0.0,0:0");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.0.0,0:0");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.1.0,0:0");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.1.1,0:0");
+		a.fuegeAufgabeHinzu("Test1", "0", "1.1.-1,0:0");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.1.1,0:0");
+		a.fuegeAufgabeHinzu("Test1", "0", "1.1.1,1:0");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.1.1,0:1");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.1.1,24:0");
+		a.fuegeAufgabeHinzu("Test1", "1", "1.1.1,0:60");
+		a.fuegeAufgabeHinzu("Test1", "true", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "ja", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "Ja", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "false", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "f", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "nein", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "0", "10.10.1000,01:00");
+		a.fuegeAufgabeHinzu("Test1", "2", "10.10.1000,01:00");
+		}
 }
